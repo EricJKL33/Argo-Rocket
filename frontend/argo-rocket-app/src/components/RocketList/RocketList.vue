2882ce7bbb6ebd809e2be07f8440642d7d3a856a
EricJKL33 <eric.jkl@icloud.com>
2024-04-26 17:56:26 +0200

    Update RocketList.scss and RocketList.vue files

--- frontend/argo-rocket-app/src/components/RocketList/RocketList.vue
+++ frontend/argo-rocket-app/src/components/RocketList/RocketList.vue
@@ -95,7 +95,7 @@ export default {
       this.rockets = response.data;
       setTimeout(() => {
         this.isLoading = false;
-      }, 3000);
+      }, 2000);
     } catch (error) {
       console.log(error);
       this.isLoading = false;
